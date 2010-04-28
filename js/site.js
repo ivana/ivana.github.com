eb72354a422067989b3701c4bdbc132a6e2665f2
Ivana Vasilj <ivasilj@gmail.com>
2010-04-28 23:48:40 +0200

    navigation hover and active

---
+++ js/site.js
@@ -0,0 +1,10 @@
+$(function(){
+
+  var currentPage = window.location.pathname.split('/')[window.location.pathname.split('/').length - 2];
+
+  switch(currentPage) {
+    case "about":
+      $('nav a[href=/about]').parent().html($('nav a[href=/about]').html() + ' =');
+      break;
+  }
+});
