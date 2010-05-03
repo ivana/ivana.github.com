1b95cd439af20648b92d0edb57a310f25fcf284c
Ivana Vasilj <ivasilj@gmail.com>
2010-05-03 23:54:49 +0200

    to execute on window load

--- js/site.js
+++ js/site.js
@@ -1,10 +1,17 @@
 $(function(){
 
-  var currentPage = window.location.pathname.split('/')[window.location.pathname.split('/').length - 2];
+  $(window).load(function(){
 
-  switch(currentPage) {
-    case "about":
-      $('nav a[href=/about]').parent().html($('nav a[href=/about]').html() + ' =');
-      break;
-  }
+    var currentPage = window.location.pathname.split('/')[window.location.pathname.split('/').length - 2];
+
+    switch(currentPage) {
+      case "about":
+        $('nav a[href=/about]').parent().html($('nav a[href=/about]').html() + ' =');
+        break;
+    }
+
+    $('body').css('min-height', screen.availHeight);
+
+  });
+  
 });
