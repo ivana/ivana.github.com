4d20ef6c5f80985f77fb557526b5f6e03a279501
Ivana Vasilj <ivasilj@gmail.com>
2010-05-04 15:19:06 +0200

    blog archive

--- js/site.js
+++ js/site.js
@@ -3,15 +3,10 @@ $(function(){
   $(window).load(function(){
 
     var currentPage = window.location.pathname.split('/')[window.location.pathname.split('/').length - 2];
-
-    switch(currentPage) {
-      case "about":
-        $('nav a[href=/about]').parent().html($('nav a[href=/about]').html() + ' =');
-        break;
-    }
+    $('nav a[href=/' + currentPage + ']').parent().html($('nav a[href=/' + currentPage + ']').html() + ' =');
 
     $('body').css('min-height', screen.availHeight);
 
   });
-  
+
 });
