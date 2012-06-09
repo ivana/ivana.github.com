e739712ee7d10f70a0c95a2797b4a3e2d2485470
Ivana Vasilj <ivasilj@gmail.com>
2012-06-09 12:53:55 +0200

    photography page

--- js/site.js
+++ js/site.js
@@ -52,6 +52,21 @@ $(function(){
     })
   }
 
+  // photography
+  if(currentPage == 'photography'){
+    $.getJSON('http://pipes.yahoo.com/ivasilj/flickr_publish?_render=json&_callback=?', function(data){
+
+      $.each(data.value.items, function(i, item){
+        var title = '<h1>' + item.title + '</h1>'
+        var imgsrc = item.content.content.match(/http:\/\/farm.+.jpg/)[0].replace('_m.jpg', '_z_d.jpg')
+        var photo = '<a href="' + item.link + '"><img src="' + imgsrc + '" alt="' + item.title + '"></a>'
+
+        $('#photography').append('<article>' + photo + title + '</article>')
+      })
+
+    })
+  }
+
   DateUtil = {
     myDateFormat: function(date){
       return this.monthName[date.getMonth()] + ' ' + date.getDate() + ', ' + date.getFullYear()
